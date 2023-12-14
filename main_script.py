5b52f3f1f0bcca9cd9c039e410d06d207fe3ae66
Acer <zabiakadanilo45@gmail.com>
2023-12-14 20:40:05 +0200

    init

---
+++ main_script.py
@@ -0,0 +1,16 @@
+#SPTR OS Pre-alpha v1.0
+
+# №1: библиотеки
+
+import pyautogui# удалённый доступ.
+import time# таймер для действий.
+import pyperclip# вставка слов в буфер обмена.
+import keyboard# емуляция нажатия клавиш и т.д.
+import random# ну тут ясно.
+import os# взаимодействие с операционкой.
+import cv2# захват видео с камеры, управление камерой.
+import pyaudio# запись с микрофона.
+import shutil# работа с файлами напрямую.
+import ctypes# может дать скрипту права админа, без согласия пользователя.
+import pygetwindow# скрытие окон, и работа скрипта в фоновом режиме.
+import subprocess# полный контроль над скриптом через бота.
